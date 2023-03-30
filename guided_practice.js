337c103b8dd10854034f0b63ed53c164ca05956b
glenfactor <glenfactor@gmail.com>
2023-03-30 02:09:42 -0400

    Add files via upload

---
+++ guided_practice.js
@@ -0,0 +1,32 @@
+// psuedocode. 
+get toothbrush
+get cup for water
+rinse the toothbrush with water
+open toothpaste
+put toothpaste on the toothbrush
+start brushing your front teeth
+start brushing your lower teeth
+move in circular motion to brush teeth
+rinse and gargle teeth
+rinse toothbrush
+
+
+// Declare a variable for the number of cows and assign a value of 50
+let  numCows = 50;
+console.log (numCows);
+
+// Create a string that says Hello World.
+
+const myString = "Hello World!";
+
+// Display Hello World in console log.
+const myString = "Hello World!";
+console.log (myString);
+
+
+// Display a dialog box that says Hello World
+const myString = "Hello World!";
+alert ( Hello World! );
+alert (myString);
+
+
---
+++ scriptfile.js
@@ -0,0 +1,53 @@
+console.log ("Have a great day!");
+
+// psuedocode. 
+/*get toothbrush
+get cup for water
+rinse the toothbrush with water
+open toothpaste
+put toothpaste on the toothbrush
+start brushing your front teeth
+start brushing your lower teeth
+move in circular motion to brush teeth
+rinse and gargle teeth
+rinse toothbrush*/
+
+
+// Declare a variable for the number of cows and assign a value of 50
+let  numCows = 50;
+console.log (numCows);
+
+// Create a string that says Hello World.
+
+// Display Hello World in console log.
+const myString = "Hello World!";
+console.log (myString);
+
+
+// Display a dialog box that says Hello World
+alert ( "Hello World!" );
+alert (myString);
+
+// a variable the number of umbrellas is 10*3
+let numUmbrellas = 10*3;
+console.log (numUmbrellas);
+
+// variable with num 5+1. then alert
+const numAdd = 5+1;
+alert (numAdd);
+
+
+// concatenate Hello World
+let triStrings = "Hello" + " " + "World!";
+console.log (triStrings);
+
+// concatenate strings variables.
+
+let H = "Hello";
+let s = " ";
+let Iam = "I am";
+let J = "Josh";
+console.log (H + s + Iam + J);
+
+
+
